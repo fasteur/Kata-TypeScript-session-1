c75e483742932122469dc05cbd425d7212d8b24f
fasteur <f.asteur@afelio.be>
2023-04-14 10:58:36 +0200

    feat: Add comments for extends operator
    Signed-off-by: fasteur <f.asteur@afelio.be>

--- conditional-type.example.ts
+++ conditional-type.example.ts
@@ -1,6 +1,7 @@
 // Dans cet exemple, nous avons défini un type Animal qui représente les deux types d'animaux possibles : "chat" et "chien". 
 // Ensuite, nous avons défini un type générique Sound qui prend un type T qui étend le type Animal. 
 // Le typage conditionnel est utilisé pour déterminer le son approprié à associer à chaque type d'animal.
+// L'opérateur extends est utilisé pour créer des contraintes de type. Il permet de spécifier que le type générique doit satisfaire une condition particulière pour être valide.
 
 type Animal = "chat" | "chien";
 
